c8ec2e141469a8feac05aa0f52c06ea3b9e64e1b
rizalshmbg <sihombingrizal18@gmail.com>
2022-06-08 20:26:52 +0700

    created checkout page along with checkout items and checkout cart functionality & completed section 9: react context continued

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './routes/home/Home'
 import Navigation from './routes/navigation/Navigation'
 import Authentication from './routes/authentication/Authentication'
 import Shop from './routes/shop/Shop'
+import Checkout from './routes/checkout/Checkout'
 
 const App = () => {
 	return (
@@ -11,6 +12,7 @@ const App = () => {
 				<Route index element={<Home />} />
 				<Route path='shop' element={<Shop />} />
 				<Route path='auth' element={<Authentication />} />
+				<Route path='checkout' element={<Checkout />} />
 			</Route>
 		</Routes>
 	)
